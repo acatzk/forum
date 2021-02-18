dea2344572814df9d9581b54b37c208248847400
Joshua Galit <joshuaimalay@gmail.com>
2021-02-18 10:27:46 +0800

    feat: nprogress loading indicator

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,11 @@
 import '~/styles/tailwind.css'
 import { AuthProvider } from '~/context/auth'
+import NProgress from '~/plugins/NProgress'
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <NProgress />
       <Component {...pageProps} />
     </AuthProvider>
   )
