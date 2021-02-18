39be2a9c18fbbbec5ca8993d04aa2db43d799595
Joshua Galit <joshuaimalay@gmail.com>
2021-02-18 10:30:31 +0800

    Revert "feat: nprogress loading indicator"
    This reverts commit dea2344572814df9d9581b54b37c208248847400.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,9 @@
 import '~/styles/tailwind.css'
 import { AuthProvider } from '~/context/auth'
-import NProgress from '~/plugins/NProgress'
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <NProgress />
       <Component {...pageProps} />
     </AuthProvider>
   )
